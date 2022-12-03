7eba0cbf20ad1812c80083781eee62e0d5c4abe9
Daniel Hajialigol <danielhajialigol@gmail.com>
2022-12-03 17:38:13 -0500

    forgot to add utils lol

---
+++ src/torch_cloudnet/utils.py
@@ -0,0 +1,23 @@
+from typing import Dict
+from pathlib import Path
+import pickle
+
+
+def save_predictions(predictions_dict: Dict, write_path: Path):
+    """
+    Method to write predictions as inputs
+    :param predictions_dict:
+    :param write_path:
+    :return:
+    """
+    with open(write_path, 'wb') as fp:
+        pickle.dump(predictions_dict, fp)
+
+
+def read_predictions(read_path: Path):
+    """
+    :param read_path:
+    :return:
+    """
+    with open(read_path, 'rb') as fp:
+        return pickle.load(fp)
